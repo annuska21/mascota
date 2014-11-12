280a923cae7ef6c1163ef8f208518d270ca3d132
annuska21 <ana.galarza@gmail.com>
2014-11-12 07:19:57 -0800

    miercoles

--- db/migrate/20141103123532_cambio_tipo_datos_shelters.rb
+++
@@ -1,9 +0,0 @@
-class CambioTipoDatosShelters < ActiveRecord::Migration
-  def up
-    change_column("shelters", "detalles_voluntariado", :text)
-  end
-
-  def down
-    change_column("shelters", "detalles_voluntariado", :boolean)
-  end
-end
